acd5f8d8e680da4f129b72f96a7230283989b11a
PaketMoloka <vladimir22092012@mail.ru>
2024-03-02 20:41:16 +0200

    fix search

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -86,7 +86,7 @@ class ProductRepository extends CommonRepository implements CommonRepositoryInte
         if (isset($data['query'])) {
             $q = $data['query'];
 
-            $db = DB::connection('sphinx')->table('idx_products_name')->toSql();
+            $db = DB::connection('sphinx')->table('idx_products_name')->whereRaw('MATCH('.$q.')')->toSql();
             dd($db);
 
             /*$categoryQuery = Category::query();
